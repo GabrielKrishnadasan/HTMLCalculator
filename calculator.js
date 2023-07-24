8d819595d0e3017c55f0efd2d1f613c9a97ddeb8
Gabe Krishnadasan <gkrishnadasan@sandiego.edu>
2023-07-24 12:11:30 -0700

    Completed enter button

--- calculator.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('keydown', function(event) {
-            // Check if the Enter key was pressed
-            if (event.keyCode === 13 || event.key === "Enter") {
-                display.value = eval(display.value);
-            }
-        });
